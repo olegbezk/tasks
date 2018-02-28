d99a2fc2e17ed55f3df78828914c0f08263e51bb
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-02-28 15:45:12 +0200

    add angular tasks list component

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks:Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+        this.tasks.push(new Task(1, "Task 1", true, "01/02/2018"))
+        this.tasks.push(new Task(2, "Task 2", false, "01/02/2018"))
+        this.tasks.push(new Task(3, "Task 3", false, "01/02/2018"))
+    }
+
+
+    onTaskChange(event, task: Task) {
+        // this.taskService.saveTask(task, event.target.checked).subscribe();
+        console.log('Task has changed');
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'label-success' : 'label-primary';
+    }
 }
